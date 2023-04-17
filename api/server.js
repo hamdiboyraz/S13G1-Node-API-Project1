fa8037eff8733ced3a07f307d23bf88fcd8dafcb
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-17 16:57:59 +0300

    Add post request

--- api/server.js
+++ api/server.js
@@ -10,18 +10,50 @@ const server = express();
 server.use(express.json());
 
 // API
+
+// GET All Users
 server.get("/api/users", (req, res) => {
-  console.log("hi");
-  console.log(controller.find());
-  controller.find().then((users) => {
-    res.json(users);
-  }).catch(err => {
-    console.error(err);
-    res.status(500).json({
-        message: "Kullanıcı bilgileri alınamadı"
+  controller
+    .find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: "Kullanıcı bilgileri alınamadı",
+      });
     });
-  });
 });
+
+// CREATE User
+server.post("/api/users", (req, res) => {
+  // console.log(req.body);
+
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    return res
+      .status(400)
+      .json({ message: "Lütfen kullanıcı için bir name ve bio sağlayın" });
+  }
+  controller
+    .insert(req.body)
+    .then((newUser) => {
+      res.status(201).json({
+        message: "Kullanıcı başarıyla oluşturuldu",
+        data: newUser,
+      });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+    });
+});
+
+server.get("/api/users/:id");
+server.delete("/api/users/:id");
+server.put("/api/users/:id");
 
 // EXPORT
 module.exports = server; // SERVERINIZI EXPORT EDİN {}
