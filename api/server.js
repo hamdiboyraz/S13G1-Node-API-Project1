0a48a925fe25834f4c459a52c24145b94a4b4e0b
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-04-17 16:09:26 +0300

    Add GET request for all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,27 @@
 // SUNUCUYU BU DOSYAYA KURUN
+// IMPORTS
+const express = require("express");
+const controller = require("./users/model");
 
-module.exports = {}; // SERVERINIZI EXPORT EDİN {}
+// INSTANCE OF EXPRESS.JS
+const server = express();
+
+// GLOBAL MIDDLEWARE
+server.use(express.json());
+
+// API
+server.get("/api/users", (req, res) => {
+  console.log("hi");
+  console.log(controller.find());
+  controller.find().then((users) => {
+    res.json(users);
+  }).catch(err => {
+    console.error(err);
+    res.status(500).json({
+        message: "Kullanıcı bilgileri alınamadı"
+    });
+  });
+});
+
+// EXPORT
+module.exports = server; // SERVERINIZI EXPORT EDİN {}
